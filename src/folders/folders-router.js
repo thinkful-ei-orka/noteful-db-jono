e6d311712749c26dc8534d8069d09c80f90a6eae
Jon <jdog992@gmail.com>
2020-06-16 03:28:26 -0400

    note/folder service

---
+++ src/folders/folders-router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const xss = require('xss')
+const FoldersService = require('./folders-service')
+
+const foldersRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeFolder = folder => ({
+    id: folder.id,
+    folder_name: xss(folder.folder_name),
+})
---
+++ src/notes/notes-router.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeNote = note => ({
+    id: note.id,
+    note_name: xss(note.note_name),
+    content: xss(note.content),
+    date_modified: note.date_modified,
+    folder_id: note.folder_id,
+})
